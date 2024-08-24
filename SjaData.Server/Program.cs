89b302f8fcf91bdd4e2952b5da31848b09e018b0
Tony Richards <trichards57@pm.me>
2024-08-24 10:34:59 +0100

    Removed unused compiled model.

--- SjaData.Server/Program.cs
+++ SjaData.Server/Program.cs
@@ -8,10 +8,13 @@ using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Compliance.Classification;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using SjaData.Model;
+using SjaData.Model.Converters;
 using SjaData.Model.DataTypes;
 using SjaData.Server.Api;
 using SjaData.Server.Data;
-using SjaData.Server.Data.Compiled;
 using SjaData.Server.Logging;
 using SjaData.Server.Model;
 using SjaData.Server.Services;
@@ -29,8 +32,7 @@ builder.Services.Configure<JsonOptions>(j =>
 
 builder.Services.AddDbContext<DataContext>(o =>
 {
-    o.UseModel(DataContextModel.Instance)
-     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddIdentityCore<User>()
@@ -52,6 +54,24 @@ builder.Services.AddSwaggerGen(o =>
 {
     o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "SjaData.Server.xml"));
     o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "SjaData.Model.xml"));
+    o.MapType<Region>(() => RegionConverter.Schema);
+    o.MapType<Trust>(() => TrustConverter.Schema);
+    o.MapType<AreaDictionary<int>>(() => new OpenApiSchema
+    {
+        Type = "object",
+        AdditionalProperties = new OpenApiSchema
+        {
+            Type = "integer",
+            Format = "int32",
+        },
+        AdditionalPropertiesAllowed = true,
+        Example = new OpenApiObject
+        {
+            ["NE"] = new OpenApiInteger(42),
+            ["LAS"] = new OpenApiInteger(21),
+        },
+        OneOf = [RegionConverter.Schema, TrustConverter.Schema],
+    });
 });
 
 var app = builder.Build();
