c3bb38f0b01785f46d770dbc6c443b11b4d576fa
Tony Richards <trichards57@pm.me>
2024-09-05 08:54:31 +0100

    Added patient count to new controller.

--- SjaData.Server/Controllers/PatientController.cs
+++ SjaData.Server/Controllers/PatientController.cs
@@ -5,8 +5,11 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web;
+using SjaData.Model;
 using SjaData.Model.DataTypes;
 using SjaData.Model.Patient;
+using SjaData.Server.Api.Model;
+using SjaData.Server.Controllers.Binders;
 using SjaData.Server.Logging;
 using SjaData.Server.Services.Interfaces;
 
@@ -37,6 +40,82 @@ public partial class PatientController(IPatientService patientService, ILogger<P
         await patientService.AddAsync(patient);
         LogPatientCreated(User.GetNameIdentifierId() ?? "Unknown", patient);
         return NoContent();
+    }
+
+    /// <summary>
+    /// Gets the patient count matching the given query.
+    /// </summary>
+    /// <param name="ifModifiedSince">The last-modified date held by the local cache.</param>
+    /// <param name="region">The region to filter the patients by.</param>
+    /// <param name="trust">The trust to filter the patients by.</param>
+    /// <param name="eventType">The event type to filter the patients by.</param>
+    /// <param name="outcome">The outcome to filter the patients by.</param>
+    /// <param name="date">The date to filter the patients by.</param>
+    /// <param name="dateType">The type of date filter to apply.  Defaults to month.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation. Resolves to the outcome of the action.</returns>
+    /// <response code="200">The patient count matching the given query.</response>
+    /// <response code="304">The count has not changed since the given date.</response>
+    /// <response code="400">The query was invalid.</response>
+    [HttpGet("count")]
+    [ProducesResponseType<PatientCount>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status304NotModified)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetHoursCount(
+        [FromHeader(Name = "If-Modified-Since")] DateTimeOffset? ifModifiedSince,
+        [FromQuery(Name = "region")] Region? region,
+        [FromQuery(Name = "trust")] Trust? trust,
+        [FromQuery(Name = "event-type")] EventType? eventType,
+        [FromQuery(Name = "outcome")] Outcome? outcome,
+        [FromQuery(Name = "date")] DateOnly? date,
+        [FromQuery(Name = "date-type")][ModelBinder(BinderType = typeof(DateTypeBinder))] DateType? dateType)
+    {
+        if (date is null && dateType is not null)
+        {
+            ModelState.AddModelError("date", "Date must be provided if date-type is specified.");
+            return ValidationProblem();
+        }
+
+        if (date is not null && dateType is null)
+        {
+            dateType = DateType.Month;
+        }
+
+        if (region is not null && trust is not null)
+        {
+            ModelState.AddModelError(string.Empty, "Only one of region or trust can be specified.");
+            return ValidationProblem();
+        }
+
+        if (ifModifiedSince.HasValue)
+        {
+            var lastModified = await patientService.GetLastModifiedAsync();
+
+            var age = lastModified - ifModifiedSince;
+
+            if (age < TimeSpan.FromSeconds(1))
+            {
+                LogPatientCountNotModified(ifModifiedSince.Value, lastModified);
+
+                return StatusCode(StatusCodes.Status304NotModified);
+            }
+        }
+
+        var count = await patientService.CountAsync(new PatientQuery
+        {
+            Date = date,
+            DateType = dateType,
+            Region = region,
+            Trust = trust,
+            EventType = eventType,
+            Outcome = outcome,
+        });
+
+        Response.GetTypedHeaders().LastModified = count.LastUpdate;
+        Response.GetTypedHeaders().CacheControl = new() { Private = true, NoCache = true };
+
+        LogPatientCountFound(count.LastUpdate, count);
+
+        return Ok(count);
     }
 
     [LoggerMessage(EventCodes.ItemFound, LogLevel.Information, "Patient count has been returned. It was last modified on {lastModified}.")]
