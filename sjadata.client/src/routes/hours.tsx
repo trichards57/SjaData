c0823354ba5f99b5515e92732ca200a69b10dc51
Tony Richards <trichards57@pm.me>
2024-09-13 09:09:56 +0100

    Added region filtering to the hours page.

--- sjadata.client/src/routes/hours.tsx
+++ sjadata.client/src/routes/hours.tsx
@@ -46,6 +46,17 @@ const nhseContractAreas: [AreaLabel, string][] = [
   ["IWAS", "Isle of Wight Ambulance Service"],
 ];
 
+const regions: [AreaLabel, string][] = [
+  ["EM", "East Midlands"],
+  ["EOE", "East of England"],
+  ["LON", "London"],
+  ["NE", "North East"],
+  ["NW", "North West"],
+  ["SE", "South East"],
+  ["SW", "South West"],
+  ["WM", "West Midlands"],
+];
+
 function calculateSum(
   counts: Partial<Record<AreaLabel, number>>,
   areas: AreaLabel[]
@@ -69,13 +80,23 @@ export function Hours({ ytd, lastMonth, month, target }: HoursProps) {
   const actualNhseAreas = nhseContractAreas.filter(([area]) =>
     ytdKeys.includes(area)
   );
+  const actualRegions = regions.filter(([area]) => ytdKeys.includes(area));
 
   const { selectedAreas, AreaCheck, toggleArea } = useSelectedAreas();
   const [expandNHSE, setExpandNHSE] = useState(false);
+  const [expandRegions, setExpandRegions] = useState(false);
 
   useEffect(() => {
     if (selectedAreas && selectedAreas.length > 0) {
-      setExpandNHSE(true);
+      setExpandNHSE(
+        selectedAreas.filter((a) =>
+          nhseContractAreas.map(([area]) => area).includes(a)
+        ).length > 0
+      );
+      setExpandRegions(
+        selectedAreas.filter((a) => regions.map(([area]) => area).includes(a))
+          .length > 0
+      );
     }
   }, [selectedAreas]);
 
@@ -84,6 +105,47 @@ export function Hours({ ytd, lastMonth, month, target }: HoursProps) {
     calculateSum(lastMonth.counts, selectedAreas)
   );
   const monthTotal = Math.round(calculateSum(month.counts, selectedAreas));
+  const nhseLastMonthTotal = Math.round(
+    calculateSum(
+      lastMonth.counts,
+      selectedAreas.length === 0
+        ? nhseContractAreas.map(([area]) => area)
+        : selectedAreas.filter((a) =>
+            nhseContractAreas.map(([area]) => area).includes(a)
+          )
+    )
+  );
+  const nhseMonthTotal = Math.round(
+    calculateSum(
+      month.counts,
+      selectedAreas.filter((a) =>
+        nhseContractAreas.map(([area]) => area).includes(a)
+      )
+    )
+  );
+  const regionsLastMonthTotal = Math.round(
+    calculateSum(
+      lastMonth.counts,
+      selectedAreas.length === 0
+        ? nhseContractAreas.map(([area]) => area)
+        : selectedAreas.filter((a) => regions.map(([area]) => area).includes(a))
+    )
+  );
+  const regionsMonthTotal = Math.round(
+    calculateSum(
+      month.counts,
+      selectedAreas.filter((a) => regions.map(([area]) => area).includes(a))
+    )
+  );
+  const nhseSelected =
+    selectedAreas.length === 0 ||
+    selectedAreas.filter((a) =>
+      nhseContractAreas.map(([area]) => area).includes(a)
+    ).length > 0;
+  const regionsSelected =
+    selectedAreas.length === 0 ||
+    selectedAreas.filter((a) => regions.map(([area]) => area).includes(a))
+      .length > 0;
 
   return (
     <>
@@ -94,24 +156,59 @@ export function Hours({ ytd, lastMonth, month, target }: HoursProps) {
             <FontAwesomeIcon icon={faHouse} /> Go Home
           </Link>
         </h3>
-        <ul className="link-boxes">
-          <li className="hours-box month">
-            <div>Last Month</div>
-            <div>{lastMonthTotal}</div>
-          </li>
-          <li className="hours-box month">
-            <div>This Month</div>
-            <div>{monthTotal}</div>
-          </li>
-          <li className="hours-box target">
-            <div>NHSE Target</div>
-            <div>{target}</div>
-          </li>
-          <li className="hours-box ytd">
-            <div>Year to Date</div>
-            <div>{hoursTotal}</div>
-          </li>
-        </ul>
+        {nhseSelected && regionsSelected && (
+          <>
+            <h3>Volunteer Activity</h3>
+            <ul className="link-boxes">
+              <li className="hours-box last-month">
+                <div>Last Month</div>
+                <div>{lastMonthTotal}</div>
+              </li>
+              <li className="hours-box month">
+                <div>This Month</div>
+                <div>{monthTotal}</div>
+              </li>
+              <li className="hours-box ytd">
+                <div>Year to Date</div>
+                <div>{hoursTotal}</div>
+              </li>
+            </ul>
+          </>
+        )}
+        {nhseSelected && !regionsSelected && (
+          <>
+            <h3>NHSE Contract</h3>
+            <ul className="link-boxes">
+              <li className="hours-box last-month">
+                <div>Last Month</div>
+                <div>{nhseLastMonthTotal}</div>
+              </li>
+              <li className="hours-box month">
+                <div>This Month</div>
+                <div>{nhseMonthTotal}</div>
+              </li>
+              <li className="hours-box target">
+                <div>Target</div>
+                <div>{target}</div>
+              </li>
+            </ul>
+          </>
+        )}
+        {!nhseSelected && regionsSelected && (
+          <>
+            <h3>Events</h3>
+            <ul className="link-boxes">
+              <li className="hours-box last-month">
+                <div>Last Month</div>
+                <div>{regionsLastMonthTotal}</div>
+              </li>
+              <li className="hours-box month">
+                <div>This Month</div>
+                <div>{regionsMonthTotal}</div>
+              </li>
+            </ul>
+          </>
+        )}
       </section>
       <section>
         <h3>Filter</h3>
@@ -124,13 +221,41 @@ export function Hours({ ytd, lastMonth, month, target }: HoursProps) {
             )}{" "}
             NHSE Contract
           </li>
-          <ul className={"area-section-list" + (expandNHSE ? " expanded" : "")}>
-            {actualNhseAreas.map(([area, description]) => (
-              <li key={area} onClick={() => toggleArea(area)}>
-                <AreaCheck area={area} /> {description}
-              </li>
-            ))}
-          </ul>
+          <li>
+            <ul
+              className={"area-section-list" + (expandNHSE ? " expanded" : "")}
+            >
+              {actualNhseAreas.map(([area, description]) => (
+                <li key={area} onClick={() => toggleArea(area)}>
+                  <AreaCheck area={area} /> {description}
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li
+            className="area-section"
+            onClick={() => setExpandRegions((s) => !s)}
+          >
+            {expandRegions ? (
+              <FontAwesomeIcon icon={faMinus} />
+            ) : (
+              <FontAwesomeIcon icon={faPlus} />
+            )}{" "}
+            Event Regions
+          </li>
+          <li>
+            <ul
+              className={
+                "area-section-list" + (expandRegions ? " expanded" : "")
+              }
+            >
+              {actualRegions.map(([area, description]) => (
+                <li key={area} onClick={() => toggleArea(area)}>
+                  <AreaCheck area={area} /> {description}
+                </li>
+              ))}
+            </ul>
+          </li>
         </ul>
         <footer>
           <p>
