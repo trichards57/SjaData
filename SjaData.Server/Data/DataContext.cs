7af27314abcd4533889cf603e1adb15be18e9f88
Tony Richards <trichards57@pm.me>
2024-09-19 19:17:41 +0100

    Removed old Identity reference.

--- SjaData.Server/Data/DataContext.cs
+++ SjaData.Server/Data/DataContext.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace SjaData.Server.Data;
