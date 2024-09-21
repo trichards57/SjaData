76eada495ed69f9ae871e143892c68e590724fa9
Tony Richards <trichards57@pm.me>
2024-09-21 09:28:13 +0100

    Removed old model.

--- SjaData.Model/DataTypes/DataClassifications.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.Compliance.Classification;
-
-namespace SjaData.Model.DataTypes;
-
-public static class DataClassifications
-{
-    public static DataClassification PatientData { get; } = new DataClassification("PatientDataTaxonomy", "PatientData");
-}
