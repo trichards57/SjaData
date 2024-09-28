3e364aef5e69ccab3bcd2b1cdfa28abd258bccc4
Tony Richards <trichards57@pm.me>
2024-09-28 12:47:34 +0100

    Setup the sign in page to only accept Microsoft accounts.

---
+++ SJAData/SJAData.Client/Validation/NonSjaEmailOnlyAttribute.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="NonSjaEmailOnlyAttribute.cs" company="Tony Richards">
+// Copyright (c) Tony Richards. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.ComponentModel.DataAnnotations;
+
+namespace SJAData.Client.Validation;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public class NonSjaEmailOnlyAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value == null)
+        {
+            return true;
+        }
+
+        var email = value.ToString();
+
+        if (email?.EndsWith("@sja.org.uk") == true)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
