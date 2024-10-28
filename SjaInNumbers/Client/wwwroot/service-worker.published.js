059f692ce21ccb9bb51c6c734dc5da4f3d7b3bcd
Tony Richards <trichards57@pm.me>
2024-10-28 17:03:51 +0000

    continuing to work on self-update behaviour.

--- SjaInNumbers/Client/wwwroot/service-worker.published.js
+++ SjaInNumbers/Client/wwwroot/service-worker.published.js
@@ -14,16 +14,14 @@ const offlineAssetsExclude = [/^service-worker\.js$/];
 async function onInstall(event) {
     console.info('Service worker: Install');
 
+    self.skipWaiting();
+
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
         .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
-
-    if (window.DotNet) {
-        window.DotNet.invokeMethodAsync('SjaInNumbers.Client', 'NewWorkerAvailable');
-    }
 }
 
 async function onActivate(event) {
